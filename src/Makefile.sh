475ddb3b9bf022fcd64d4a05d99b00a29e385c59
Rob174 <robin.128.456@gmail.com>
2021-03-17 19:31:58 +0100

    Makefile temporaire ? ok

---
+++ src/Makefile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Specify a name for your grammar:";
+read grammar;
+echo "${grammar} is your grammar, now specify a name for your parser";
+read parser;
+python main.py ${grammar} -o ${parser};
+nom_fichier="${parser}.c";
+echo "${nom_fichier}"
+gcc -Wall -o ${parser} -c  ${nom_fichier};
