a9476ae45b7577890d9856c1388c2de87cef3cf3
Rob174 <robin.128.456@gmail.com>
2021-03-18 13:48:37 +0100

    Makefile python3

--- src/Makefile.sh
+++ src/Makefile.sh
@@ -4,7 +4,7 @@ echo "Specify a name for your grammar:";
 read grammar;
 echo "${grammar} is your grammar, now specify a name for your parser";
 read parser;
-python main.py ${grammar} -o ${parser};
+python3 main.py ${grammar} -o ${parser};
 nom_fichier="${parser}.c";
 echo "${nom_fichier}"
 gcc -Wall -o ${parser}   ${nom_fichier};
