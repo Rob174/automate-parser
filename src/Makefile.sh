1f52420c14b671e45ae155b2d312fd7291308e38
Rob174 <robin.128.456@gmail.com>
2021-03-18 13:58:39 +0100

    Changement message makefile

--- src/Makefile.sh
+++ src/Makefile.sh
@@ -2,7 +2,7 @@
 
 echo "Specify a name for your grammar:";
 read grammar;
-echo "${grammar} is your grammar, now specify a name for your parser";
+echo "${grammar} is your grammar file path, now specify a path for your parser";
 read parser;
 python3 main.py ${grammar} -o ${parser};
 nom_fichier="${parser}.c";
